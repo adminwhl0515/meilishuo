c68b28b77f1ca0b00e227bdaf68df50f9108a253
adminwhl0515 <973506381@qq.com>
2018-05-31 10:05:39 +0800

    初始化网页

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+	
+}
+function minhieght(arr){
+	var min = arr[i].top;
+	var index = 0;
+	for(var i = 0,len = arr.length;i < len;i++){
+		if(min > arr[i].top){
+			min = arr[i].top;
+		}
+	}
+}
+function pbl(ele,child,)
